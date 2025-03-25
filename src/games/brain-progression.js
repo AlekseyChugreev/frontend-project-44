afd766a22316119be6876a6ba8078c24b7d4e819
Aleksey Chugreev <a.a.chugreev@gmail.com>
2025-03-25 21:07:03 +0300

    brain-prime fix

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,15 +3,15 @@ import { commonLogic } from '../index.js';
 export { game };
 
 const getProgression = () => {
-    const progressions = [
-        [5, 7, 9, 11, 13, 15, 17, 19, 21, 23],
-        [2, 5, 8, 11, 14, 17, 20, 23, 26, 29],
-        [14, 19, 24, 29, 34, 39, 44, 49, 54, 59],
-        [4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
-        [0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
-        [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-    ];
-    return progressions[_.random(0, progressions.length - 1)];
+    const progression = [];
+    let startNumber = _.random(0, 100);
+    const addedNumber = _.random(2, 7);
+    for (let i = 0; i < 10; i++) {
+        const resultnumber = startNumber + addedNumber;
+        progression.push(resultnumber);
+        startNumber = resultnumber;
+    }
+    return progression;
 };
 
 const information = 'What number is missing in the progression?';
