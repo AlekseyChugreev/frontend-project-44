40126923238ac8bec910b77f87a037e3ceecb220
Aleksey Chugreev <a.a.chugreev@gmail.com>
2025-03-27 15:40:11 +0300

    fix bin src

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,14 +1,15 @@
-import _ from 'lodash'
-import { commonLogic } from '../index.js'
-export { game }
+import _ from 'lodash';
+import commonLogic from '../index.js';
 
-const information = 'Answer "yes" if the number is even, otherwise answer "no".'
+const information = 'Answer "yes" if the number is even, otherwise answer "no".';
 const localLogic = () => {
-  const number = _.random(0, 100)
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no'
-  return [number, correctAnswer]
-}
+  const number = _.random(0, 100);
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  return [number, correctAnswer];
+};
 
 const game = () => {
-  commonLogic(information, localLogic)
-}
+  commonLogic(information, localLogic);
+};
+
+export default game;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import readlineSync from 'readline-sync'
-export { commonLogic }
+import readlineSync from 'readline-sync';
 
 const commonLogic = (information, localLogic) => {
-  console.log('Welcome to the Brain Games!')
-  const userName = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${userName}!`)
-  console.log(information)
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(information);
 
-  let flag = true
-  for (let i = 0; i < 3; i++) {
-    const [question, correctAnswer] = localLogic()
-    console.log(`Question: ${question}`)
-    const userAnswer = readlineSync.question('Your answer: ')
+  let flag = true;
+  for (let i = 0; i < 3; i += 1) {
+    const [question, correctAnswer] = localLogic();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-      flag = false
-      break
-    }
-    console.log('Correct!')
-  }
-  if (flag) console.log(`Congratulations, ${userName}!`)
-  else console.log(`Let's try again, ${userName}!`)
-}
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      flag = false;
+      break;
+    };
+    console.log('Correct!');
+  };
+  if (flag) console.log(`Congratulations, ${userName}!`);
+  else console.log(`Let's try again, ${userName}!`);
+};
+
+export default commonLogic;
